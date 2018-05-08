64ea4e2eb55775f77bf46157d96ae7e6aa613ae8
travis88 <hellsballs99@gmail.com>
2018-05-08 05:26:50 +0300

    added favicon

--- headlines.py
+++ headlines.py
@@ -13,23 +13,24 @@ RSS_FEED = {
 @app.route('/')
 @app.route('/<publication>')
 def get_news(publication='yandex'):
-    try:
-        feed = feedparser.parse(RSS_FEED[publication]
-                                            .encode('utf-8').decode('utf-8'),)
-        first_article = feed['entries'][0]
-        return """
-        <html>
-            <body>
-                <h1> Yandex Movies Headlines</h1>
-                <b>{0}</b><br/>
-                <i>{1}</i><br/>
-                <p>{2}</p><br/>
-            </body>
-        </html>""".format(first_article.get('title'), 
-                        first_article.get('published'),
-                        first_article.get('description'))
-    except:
-        return 'exception'
+    feed = feedparser.parse(RSS_FEED[publication].encode('utf-8').decode('utf-8'),)
+    first_article = feed['entries'][0]
+    return """
+    <html>
+        <head>
+            <link id="site-favicon" 
+                  href="http://www.allitebooks.com/wp-content/themes/allitebooks/images/favicon.ico" 
+                  rel="shortcut icon" type="image/x-icon">
+        </head>
+        <body>
+            <h1> Yandex Movies Headlines</h1>
+            <b>{0}</b><br/>
+            <i>{1}</i><br/>
+            <p>{2}</p><br/>
+        </body>
+    </html>""".format(first_article.get('title'), 
+                      first_article.get('published'),
+                      first_article.get('description'))
 
 if __name__ == '__main__':
     app.run(debug=True)
