878df2a6bdb9769e6c6910f1a409acf120001c28
travis88 <hellsballs99@gmail.com>
2018-05-09 10:37:20 +0300

    weather added

--- headlines.py
+++ headlines.py
@@ -1,4 +1,8 @@
 import feedparser
+import json
+import os
+import urllib
+from urllib.request import urlopen
 from flask import Flask, render_template, request
 
 
@@ -12,16 +16,38 @@ RSS_FEED = {
     "cnews": "http://www.cnews.ru/inc/rss/news_top.xml",
     "nasa": "https://www.nasa.gov/rss/dyn/earth.rss"
 }
+OPEN_WEATHER_MAP_KEY = os.environ.get('OPEN_WEATHER_MAP_KEY')
 
-@app.route("/")
+
+def get_weather(query):
+    api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
+    query = urllib.parse.quote(query)
+    url = api_url.format(query, OPEN_WEATHER_MAP_KEY)
+    r = urlopen(url)
+    data = r.read().decode(r.info().get_param('charset') or 'utf-8')
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get('weather'):
+        weather = {
+            'description': parsed['weather'][0]['description'],
+            'temperature': parsed['main']['temp'],
+            'city': parsed['name']
+        }
+    return weather
+
+
+@app.route("/", methods=['GET', 'POST'])
 def get_news():
-    query = request.args.get("publ")
-    if not query or query.lower() not in RSS_FEED:
+    query = request.form.get('publ')
+    if not query or query not in RSS_FEED:
         publ = 'yandex'
     else:
         publ = query.lower()
     feed = feedparser.parse(RSS_FEED[publ])
-    return render_template("home.html", articles=feed['entries'])
+    weather = get_weather('Moscow,RU')
+    return render_template("home.html", 
+                           articles=feed['entries'], 
+                           weather=weather)
 
 if __name__ == '__main__':
     app.run(debug=True)
